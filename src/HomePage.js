c51514c4b323db5a13f9267c665309d157d41a73
matheus <mat.feijo2506@gmail.com>
2022-01-09 11:40:42 -0400

    futebol

---
+++ src/HomePage.js
@@ -0,0 +1,41 @@
+import * as React from "react"
+import reactDom from "react-dom";
+import App from "./App";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import "./homePage.css";
+
+export function HomePage(){
+
+
+    return(
+
+        <div className="homePage">
+        
+
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+           
+          </Toolbar>
+        </AppBar>
+      </Box>
+            
+           
+        </div>
+    )
+}
